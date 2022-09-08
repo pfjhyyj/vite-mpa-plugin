95b6dcbc9d677696eadd5f1b0479d9fb214fa99f
chipunderblade <pfjhyyj@qq.com>
2022-09-08 21:48:58 +0800

    fix bugs found on vite 3.x

--- examples/mpa-project/vite.config.ts
+++ examples/mpa-project/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import mpaPlugin from "../../src/index";
+import react from "@vitejs/plugin-react";
+import mpa from "../../src/index";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    mpaPlugin(),
-    reactRefresh()
+    mpa(),
+    react()
   ]
 });

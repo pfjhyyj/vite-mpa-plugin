5282b8cef5799a0f3b15ac784ad21e8cbc89f669
chipunderblade <pfjhyyj@qq.com>
2023-01-16 12:53:39 +0800

    change to pnpm && update vite

--- examples/mpa-project/vite.config.ts
+++ examples/mpa-project/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import mpa from "../../src/index";
 
 // https://vitejs.dev/config/
